7c8adb351b89555eeeadc8e45eb9395ad0cce438
Nils Tonnätt <nils.tonnaett@campus.tu-berlin.de>
2021-03-10 14:45:30 +0100

    Added scripts for creating symlinks in /usr/local/bin to different SC versions!

---
+++ SHELL/supercollider_nogui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo ln -f -s ~/Development/supercollider/build/lang/sclang /usr/local/bin/sclang
+sudo ln -f -s ~/Development/supercollider/build/server/scsynth/scsynth /usr/local/bin/scsynth
+sudo ln -f -s ~/Development/supercollider/build/server/supernova/supernova /usr/local/bin/supernova
+sudo ln -f -s ~/Development/supercollider/build/editors/sc-ide/scide /usr/local/bin/scide
+
