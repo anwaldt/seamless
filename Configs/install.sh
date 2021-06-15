5a8792b6b2795715a8ce7015030661368dbab8e0
Nils Tonnätt <nils.tonnaett@posteo.de>
2021-06-15 14:59:15 +0200

    [Meson] Configs

--- Configs/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -m 755 -p /etc/hufo
-install -o root -g root -m 644 aj-snapshot/* /etc/hufo
--- Configs/main_config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ln -s /etc/hufo/aj-snapshot/en325_riviera_jack_connections.xml /etc/hufo/aj_snapshot_config.xml
