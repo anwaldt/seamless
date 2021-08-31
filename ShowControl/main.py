2c36902bd5e59c51c8fb775871618ada8c5aca7d
Nils Tonnätt <nils.tonnaett@posteo.de>
2021-08-31 17:36:34 +0200

    [ShowControl] Load schedule from YAML file

--- ShowControl/main.py
+++ ShowControl/main.py
@@ -1,23 +1,31 @@
 from oscpy.client import OSCClient
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+import yaml
 
+address = "127.0.0.1"
+port = 8000
+
+reaper = OSCClient(address, port)
 
 def play(reaper: OSCClient):
     reaper.send_message(b'/play', [1])
 
+def load_show_control():
+    with open('example.yml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        return data
+
+def add_jobs_to_scheduler(jobs, scheduler):
+    for job in jobs:
+        scheduler.add_job(play, 'date', run_date=job['time'], args=[reaper])
 
 def main():
-    address = "127.0.0.1"
-    port = 8000
-
-    reaper = OSCClient(address, port)
-
     sched = BlockingScheduler()
 
-    exec_time = datetime(2021, 8, 31, 16, 52)
+    jobs = load_show_control()
+    add_jobs_to_scheduler(jobs, sched)
 
-    job = sched.add_job(play, 'date', run_date=exec_time, args=[reaper])
     sched.start()
 
 if __name__ == "__main__":
