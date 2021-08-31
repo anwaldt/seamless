1bd7718b250b75a5090d0d9124541a5af091fd04
Nils Tonnätt <nils.tonnaett@posteo.de>
2021-08-31 16:52:38 +0200

    [ShowControl] Schedule job

--- ShowControl/main.py
+++ ShowControl/main.py
@@ -1,22 +1,23 @@
 from oscpy.client import OSCClient
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+
 
 def play(reaper: OSCClient):
     reaper.send_message(b'/play', [1])
+
 
 def main():
     address = "127.0.0.1"
     port = 8000
 
     reaper = OSCClient(address, port)
-    play(reaper)
 
-    sched = BackgroundScheduler()
+    sched = BlockingScheduler()
 
-    exec_time = datetime(2021, 8, 31, 15, 21)
+    exec_time = datetime(2021, 8, 31, 16, 52)
 
-    job = sched.add_date_job(play, exec_time, reaper)
+    job = sched.add_job(play, 'date', run_date=exec_time, args=[reaper])
     sched.start()
 
 if __name__ == "__main__":
