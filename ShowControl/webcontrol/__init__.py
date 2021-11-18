59df2932b56744a6c4b03f4c3449d7047c966647
Nils Tonnätt <nils.tonnaett@posteo.de>
2021-11-18 15:39:39 +0100

    [WebControl] Remove blog

--- ShowControl/webcontrol/__init__.py
+++ ShowControl/webcontrol/__init__.py
@@ -37,9 +37,6 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
-
     from . import showcontrol
     app.register_blueprint(showcontrol.bp)
     app.add_url_rule('/', endpoint='index')
