d03bf7138de5d406604d2ba43489fdf46c3f313b
Nils Tonnätt <nils.tonnaett@posteo.de>
2022-01-20 12:33:20 +0100

    [Reverb] Create Extension directory if not already created

--- Faust/faust-build.sh
+++ Faust/faust-build.sh
@@ -4,6 +4,7 @@ faust2jackconsole reverb/FoaReverb.dsp
 
 faust2supercollider -ks -sn -noprefix reverb/FoaReverb.dsp
 
+mkdir -p ~/.local/share/SuperCollider/Extensions/Faust/
 cp reverb/*.sc ~/.local/share/SuperCollider/Extensions/Faust/
 cp reverb/*.so ~/.local/share/SuperCollider/Extensions/Faust/
 cp reverb/*.schelp ~/.local/share/SuperCollider/Extensions/Faust/
