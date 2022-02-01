50fa6ef8b99326c2c9f511046850818ef98eef4e
Nils Tonnätt <nils.tonnaett@posteo.de>
2022-02-01 14:36:17 +0100

    Read blocks

--- ShowControl/schedule_generator.py
+++ ShowControl/schedule_generator.py
@@ -4,6 +4,8 @@ import os
 def main():
     output_file = "schedule.yml"
     track_files = ""
+    track_padding_time = 20
+
     for file in os.listdir("tracks"):
         if file.endswith(".yml"):
             with open(os.path.join('tracks', file)) as f:
@@ -11,6 +13,21 @@ def main():
     tracks = yaml.load(track_files, Loader=yaml.FullLoader)
     for track in tracks:
         print(track['title'])
+
+    for file in os.listdir("blocks"):
+        if file.endswith(".yml"):
+            with open(os.path.join('blocks', file)) as f:
+                block_files += f.read()
+    blocks = yaml.load(block, Loader=yaml.FullLoader)
+    for block in blocks:
+        print(track['title'])
+
+    N = len(blocks['tracks'])
+    for i in range(N):
+        blocks['tracks'][i+1]
+        print(blocks['tracks'][i+1])
+
+
 
 if __name__ == "__main__":
     main()
