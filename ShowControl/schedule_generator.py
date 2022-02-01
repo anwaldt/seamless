cd20d485d4248ba973c0bfa404fa0dea519dfcf4
Nils Tonnätt <nils.tonnaett@posteo.de>
2022-02-01 13:52:28 +0100

    [ShowControl] Fix opening of track files in schedule generator

--- ShowControl/schedule_generator.py
+++ ShowControl/schedule_generator.py
@@ -3,13 +3,14 @@ import os
 
 def main():
     output_file = "schedule.yml"
-
+    track_files = ""
     for file in os.listdir("tracks"):
         if file.endswith(".yml"):
-            with open(file) as f:
-                track_files += f
+            with open(os.path.join('tracks', file)) as f:
+                track_files += f.read()
     tracks = yaml.load(track_files, Loader=yaml.FullLoader)
-
+    for track in tracks:
+        print(track['title'])
 
 if __name__ == "__main__":
     main()
