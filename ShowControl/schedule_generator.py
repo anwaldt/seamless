f8f966a61503a0a10ca9d6b5e06c4dc3cc9bfc78
Nils Tonnätt <nils.tonnaett@posteo.de>
2022-02-08 16:50:05 +0100

    [SchedGenerator] Print schedule.yml

--- ShowControl/schedule_generator.py
+++ ShowControl/schedule_generator.py
@@ -1,11 +1,22 @@
 import yaml
 import os
+from datetime import datetime, timedelta
+
+def writeEntry(file, _hour, _minute, _secs, _audio_idx, _video_idx):
+    file.write(f"- audio_index: {_audio_idx}\n"
+               f"  command: play\n"
+               f"  day_of_week: 0,1,2,3,4,5,6\n"
+               f"  hour: {_hour}\n"
+               f"  minute: {_minute}\n"
+               f"  second: {_secs}\n"
+               f"  video_index: {_video_idx}\n")
 
 def main():
-    output_file = "schedule.yml"
+    startzeit = datetime(2022, 2, 1, 9, 0, 0)
+    output_file = "schedule_new.yml"
     track_files = ""
     block_files = ""
-    track_padding_time = 20
+
 
     for file in os.listdir("tracks"):
         if file.endswith(".yml"):
@@ -13,31 +24,49 @@ def main():
                 track_files += f.read()
     tracks = yaml.load(track_files, Loader=yaml.FullLoader)
 
-    print('Tracks:')
-    for track, info in tracks.items():
-        print(info['title'])
-        info['duration']['min'] = info['duration']['min'] + 1
-        info['duration']['sec'] = 0
-
     for file in os.listdir("blocks"):
         if file.endswith(".yml"):
             with open(os.path.join('blocks', file)) as f:
                 block_files += f.read()
     blocks = yaml.load(block_files, Loader=yaml.FullLoader)
 
-    print('Blocks:')
-    block_template = []
+    with open('blockplan.yml') as f:
+        blockplan = yaml.load(f.read(), Loader=yaml.FullLoader)
 
-    for value in blocks.values():
-        N = len(value)
+    N_bp = len(blockplan['default']['blocks'])
+    blockstart = startzeit
+
+    for i in range(N_bp):
+        blockname = blockplan['default']['blocks'][i+1]
+        # print(f"{i+1}. blockname: {blockname}")
+        block = blocks[blockname]
+        # print(
+        # f"Block Length: {block['length']} minutes\n"
+        # f"Track Padding: {block['track_padding']} seconds"
+        # )
+
+        N = len(block['tracks'])
+
+        trackstart = blockstart
         for i in range(N):
-            title_name = value[i+1]
-            duration = tracks[title_name]['duration']
+            title_name = block['tracks'][i+1]
+            _audio_idx = tracks[title_name]['audio_index']
+            _video_idx = tracks[title_name]['video_index']
+            _minutes = tracks[title_name]['duration']['minutes']
+            _seconds = tracks[title_name]['duration']['seconds']
 
-            block_template[i]['title']
-            print(f"{i}: {title_name} ({duration})")
+            print(f"- audio_index: {_audio_idx}\n"
+               f"  command: play\n"
+               f"  day_of_week: 0,1,2,3,4,5,6\n"
+               f"  hour: {trackstart.hour}\n"
+               f"  minute: {trackstart.minute}\n"
+               f"  second: {trackstart.second}\n"
+               f"  video_index: {_video_idx}")
 
-
+            trackstart = trackstart \
+                            + timedelta(minutes=_minutes, seconds=_seconds) \
+                            + timedelta(seconds = block['track_padding'])
+        blockstart = blockstart + timedelta(minutes=block['length'])
 
 
 if __name__ == "__main__":
