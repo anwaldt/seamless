e20a31c2bee5d760c689ea6646670d0e2caeb1d7
Nils Tonnätt <nils.tonnaett@posteo.de>
2022-02-08 16:53:46 +0100

    [SchedGenerator] Write file

--- ShowControl/schedule_generator.py
+++ ShowControl/schedule_generator.py
@@ -36,37 +36,45 @@ def main():
     N_bp = len(blockplan['default']['blocks'])
     blockstart = startzeit
 
-    for i in range(N_bp):
-        blockname = blockplan['default']['blocks'][i+1]
-        # print(f"{i+1}. blockname: {blockname}")
-        block = blocks[blockname]
-        # print(
-        # f"Block Length: {block['length']} minutes\n"
-        # f"Track Padding: {block['track_padding']} seconds"
-        # )
+    with open(output_file, "w") as out_file:
+        for i in range(N_bp):
+            blockname = blockplan['default']['blocks'][i+1]
+            # print(f"{i+1}. blockname: {blockname}")
+            block = blocks[blockname]
+            # print(
+            # f"Block Length: {block['length']} minutes\n"
+            # f"Track Padding: {block['track_padding']} seconds"
+            # )
 
-        N = len(block['tracks'])
+            N = len(block['tracks'])
 
-        trackstart = blockstart
-        for i in range(N):
-            title_name = block['tracks'][i+1]
-            _audio_idx = tracks[title_name]['audio_index']
-            _video_idx = tracks[title_name]['video_index']
-            _minutes = tracks[title_name]['duration']['minutes']
-            _seconds = tracks[title_name]['duration']['seconds']
+            trackstart = blockstart
+            for i in range(N):
+                title_name = block['tracks'][i+1]
+                _audio_idx = tracks[title_name]['audio_index']
+                _video_idx = tracks[title_name]['video_index']
+                _minutes = tracks[title_name]['duration']['minutes']
+                _seconds = tracks[title_name]['duration']['seconds']
 
-            print(f"- audio_index: {_audio_idx}\n"
-               f"  command: play\n"
-               f"  day_of_week: 0,1,2,3,4,5,6\n"
-               f"  hour: {trackstart.hour}\n"
-               f"  minute: {trackstart.minute}\n"
-               f"  second: {trackstart.second}\n"
-               f"  video_index: {_video_idx}")
+                print(f"- audio_index: {_audio_idx}\n"
+                   f"  command: play\n"
+                   f"  day_of_week: 0,1,2,3,4,5,6\n"
+                   f"  hour: {trackstart.hour}\n"
+                   f"  minute: {trackstart.minute}\n"
+                   f"  second: {trackstart.second}\n"
+                   f"  video_index: {_video_idx}")
 
-            trackstart = trackstart \
-                            + timedelta(minutes=_minutes, seconds=_seconds) \
-                            + timedelta(seconds = block['track_padding'])
-        blockstart = blockstart + timedelta(minutes=block['length'])
+                writeEntry(out_file,
+                           trackstart.hour,
+                           trackstart.minute,
+                           trackstart.second,
+                           _audio_idx,
+                           _video_idx)
+
+                trackstart = trackstart \
+                                + timedelta(minutes=_minutes, seconds=_seconds) \
+                                + timedelta(seconds = block['track_padding'])
+            blockstart = blockstart + timedelta(minutes=block['length'])
 
 
 if __name__ == "__main__":
